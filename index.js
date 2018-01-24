6f650e2904d114d50cd01ce93439f8777b840814
AllA <allagorik2901@gmail.com>
2018-01-24 16:51:53 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function depthFirstSearch(rootNode, vertices, edges) {
+    let stack = []
+    stack.push(rootNode)
+    let visited = [rootNode]
+
+    while(stack.length != 0) {
+      let xyz = stack.pop()
+      if(!xyz.discovered) {
+        xyz.discovered = true
+        findAdjacent(xyz.name, vertices, edges).forEach(function(node) {
+          visited.push(node)
+          stack.push(node)
+        })
+      }
+    }
+    return visited;
+}
+
+function findAdjacent(nodeName, vertices, edges) {
+  return edges.filter(function(edge) {
+    return edge.includes(nodeName)
+  }).map(function(edge) {
+    return edge.filter(function(node) {
+      return (node != nodeName)
+    })[0]
+  }).map(function(name) {
+    return findNode(name, vertices)
+  }).filter(function(node) {
+    return !node.discovered
+  })
+}
+
+function findNode(nodeName, vertices) {
+  return vertices.find(function(vertex) {
+    return vertex.name == nodeName
+  })
+}
+
+
+
